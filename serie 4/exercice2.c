63d18f6385d7c5f86c5bbf7d3c52543b9dd91038
anasouk <anas.oukassou@gmail.com>
2017-05-10 14:33:07 +0100

    Create exercice2.c

---
+++ serie 4/exercice2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a;
+	double b,c;
+	b = 3.5;
+	scanf("%d",&a);
+	c = (a*2)-(b*3);
+	printf("La valeur de A: %d \n",a);
+	printf("La valeur de B: %f \n",b);
+	printf("La valeur de C: %f \n",c);
+}
